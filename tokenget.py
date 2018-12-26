e61034efc5c6db9f14de05da1eb3d333a464fc3e
dsong <dsong.cc@gmail.com>
2018-12-26 21:25:02 +0800

    添加了headers装饰

--- tokenget.py
+++ tokenget.py
@@ -4,26 +4,30 @@ import re
 import requests
 from bs4 import BeautifulSoup
 def GetToken():
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': 'bbs_sid=0p217a599bm08h15siu4ng1hs4; UM_distinctid=167e969062dcc-0142d7602e0578-77103a42-100200-167e969062e4d3; CNZZDATA1274423427=1216749288-1545806831-%7C1545825191; cookie_test=dHTwtz0Oy2dRg_2FHxYTJmgOPTVht8EfipD9nbz5N_2BD5DnYRhs',
+        'Host': 'www.dtpwxn.top',
+        'Referer': 'http://www.dtpwxn.top/?forum-1.htm',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3649.0 Safari/537.36',
+    }
     id = 2049
-
     index = 'http://www.dtpwxn.top/?forum-1.htm'
-    index_html = requests.get(index).text
-    
+    index_html = requests.get(index,headers=headers).text 
     index_id = re.search(r'/?-\d{4}',index_html)#得到id
-
     now_id = int(re.search(r'[^-]{4}',index_id.group()).group()) #去掉-
-    
-
     mo_time = re.search(r'\d{1,2}[\u4e00-\u9fa5]{3}',index_html).group() #得到时间
-    
-    print('此 Token在 %s 更新'% mo_time)
-    
+    print('更新时间：')
+    print('>>>>此 Token在 %s 更新<<<<'% mo_time)
     if now_id > id:
-    
         id = now_id
-        
         url = 'http://www.dtpwxn.top/?thread-%s.htm'% id
-        html = requests.get(url).text
+        html = requests.get(url,headers=headers).text
     
         token = re.search(r'[a-z0-9]{96}',html)
         T = token.group()
@@ -31,6 +35,5 @@ def GetToken():
     else:
         return '暂时无更新'
 
-    
 GetToken()
 
